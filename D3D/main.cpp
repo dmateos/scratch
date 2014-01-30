de5f9d22c6c959b4773ea16bb428329f835adca4
Daniel Mateos <daniel@mateos.cc>
2014-01-31 00:54:02 +1030

    fps

--- D3D/main.cpp
+++ D3D/main.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void _update_fps_counter (GLFWwindow* window) {
+	static double previous_seconds = glfwGetTime ();
+	static int frame_count;
+	double current_seconds = glfwGetTime ();
+	double elapsed_seconds = current_seconds - previous_seconds;
+	if (elapsed_seconds > 0.25) {
+		previous_seconds = current_seconds;
+		double fps = (double)frame_count / elapsed_seconds;
+		char tmp[128];
+		sprintf (tmp, "opengl @ fps: %.2lf", fps);
+		glfwSetWindowTitle (window, tmp);
+		frame_count = 0;
+	}
+	frame_count++;
+}
+
 GLuint make_shader(const char *path, GLenum stype) {
 	int length;
 	GLchar *data = (GLchar*)file_contents(path, &length);
@@ -111,8 +127,9 @@ int main(int argc, char **argv) {
 
 	glm::mat4 projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
 
-
 	while(!glfwWindowShouldClose(window)) {
+		_update_fps_counter(window);
+
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
