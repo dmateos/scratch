ba50722438725e7fae7cdc069be38aefc6fdbe28
Daniel Mateos <daniel@mateos.cc>
2014-02-02 03:56:58 +1030

    more network code

--- D3D/src/mserver/main.cpp
+++ D3D/src/mserver/main.cpp
@@ -28,10 +28,23 @@ struct packet {
 	void *data;
 };
 
-void *handle_client(void *clientid) {
-	int *cid = (int*)clientid;
-	printf("new thread %d\n", cid);
+struct thread_args {
+	int client_sd;
+	sockaddr_in client_addr;
+};
+
+void *handle_client(void *targs) {
+	thread_args *args = (thread_args*)targs;
+	printf("new thread %d\n", args->client_sd);
 	while(true) {
+		packet hello_packet;
+		hello_packet.cmd = HELLO;
+		hello_packet.length = 0;
+		hello_packet.data = NULL;
+
+		if(send(args->client_sd, (void*)&hello_packet, sizeof(hello_packet), 0) == -1) {
+			printf("couldnt hello msg to client\n");
+		}
 		sleep(10);
 	}
 	pthread_exit(NULL);
@@ -41,6 +54,7 @@ int main(int argc, char **argv) {
 	int sockfd;
 	struct sockaddr_in servaddr;
 	pthread_t threads[32];
+	thread_args args[32];
 
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		printf("could not make socket fd\n");
@@ -62,7 +76,9 @@ int main(int argc, char **argv) {
 		int client = accept(sockfd, (struct sockaddr*)&client_addr, &client_addr_length);;
 		printf("new client connection\n");
 
-		int rc = pthread_create(&threads[i], NULL, handle_client, (void*)client);
+		args[i].client_sd = client;
+		args[i].client_addr = client_addr;
+		int rc = pthread_create(&threads[i], NULL, handle_client, (void*)&args[i]);
 		if(rc) {
 			printf("could not make thread\n");
 			exit(1);
